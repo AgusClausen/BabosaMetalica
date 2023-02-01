dada77d0c5bfb39fe4a9390498733262b0e168f8
juanba874 <123997357+juanba874@users.noreply.github.com>
2023-01-31 21:51:36 -0300

    Versión 0.16
    Se incorporó vida a los enemigos y daño a las balas.
    se utiliza un clock de 100 ms para determinar si la colisión le va a hacer daño al enemigo o no

---
+++ BabosaMetalica 0.16/character.h
@@ -0,0 +1,24 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "Shot.h"
+#include <SFML/System/Clock.hpp>
+using namespace sf;
+
+class character {
+	Texture t;
+	Sprite s;
+	Clock m_clock;
+	bool alive=true;
+public:
+	character();
+	void update();
+	void draw(RenderWindow &w);
+	bool mustshoot();
+	Shot shotgen();
+};
+
+#endif
+
---
+++ BabosaMetalica 0.16/enemies.h
@@ -0,0 +1,26 @@
+#ifndef ENEMIES_H
+#define ENEMIES_H
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+using namespace sf;
+class enemies {
+	Texture et;
+	float hp=100;
+	bool alive=true;
+	Clock e_clock;
+public:
+	Sprite es;
+	enemies();
+	void update();
+	void get_damage(float);
+	void draw(RenderWindow &w);
+	Vector2f seePos(){
+		return es.getPosition();
+	}
+	bool is_alive();
+};
+
+
+
+#endif
