c7bbd77142d57da276fee5d977a07fa3b2c0fd09
AgusClausen <123996495+AgusClausen@users.noreply.github.com>
2023-03-06 00:13:21 -0300

    Version 0.75
    Añadido generacion de enemigos
    Falta:
    *El personaje se muera
    *Binario
    *Sonidos
    *Pantalla con Scores

---
+++ BabosaMetalica 0.75/mapa.h
@@ -0,0 +1,17 @@
+#ifndef MAPA_H
+#define MAPA_H
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
+using namespace sf;
+class mapa {
+	Texture t;
+	Sprite s;
+public:
+	mapa();
+	void update(Vector2f pos_mapa);
+	void draw(RenderWindow &w);
+	Vector2f coordenadas();
+};
+#endif
---
+++ BabosaMetalica 0.75/sprites.cpp
@@ -0,0 +1,18 @@
+#include "sprites.h"
+
+sprites::sprites() {
+	tenemy1.loadFromFile("zombie.png");
+	tshotgun.loadFromFile("shotgun.png");
+	tg1.loadFromFile("g1.png");
+	tg5.loadFromFile("g5.png");
+	
+	sshotgun.setTexture(tshotgun);
+	sg1.setTexture(tg1);
+	sg5.setTexture(tg5);
+	
+	
+}
+
+Texture sprites::get_enemy_texture (){
+	return tenemy1;
+}
