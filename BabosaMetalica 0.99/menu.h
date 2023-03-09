2556a735704517afa422775152c3a030939a8d47
AgusClausen <123996495+AgusClausen@users.noreply.github.com>
2023-03-09 02:02:12 -0300

    Version 0.99
    Añadido archivo binario con records
    Falta:  Limpiar un poco

---
+++ BabosaMetalica 0.99/menu.h
@@ -0,0 +1,38 @@
+#ifndef MENU_H
+#define MENU_H
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/System/Clock.hpp>
+#include "character.h"
+#include <vector>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Text.hpp>
+using namespace std;
+using namespace sf;
+
+class menu {
+	Texture t1,t2,t3;
+	Sprite s1,s2,s3;
+	bool ready;
+	int points;
+	vector <int> v;
+	vector <Text> vt;
+	Text rec;
+	Font fuente;
+	Text tscore;
+	int iscore;
+	
+public:
+	menu();
+	bool start(Clock game_time,int game_kills,bool dead);
+	void draw_death_screen (RenderWindow &w);
+	void draw_main_menu (RenderWindow &w);
+	void draw_records_screen (RenderWindow &w);
+	void update(int score);
+	void delete_records();
+
+};
+
+#endif
+
